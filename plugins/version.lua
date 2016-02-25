913b498a6855dc4077b0deaf2b525ced159dfc74
5620g <abolfazlsaedi99@gmail.com>
2016-02-25 00:15:32 -0800

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+  return 'v8'.. VERSION .. [[ 
+ 
+  Developer : 
+  
+  Channel : 
+  
+  Open git : 
+  
+  NOD32 VERSION 8
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; 
+--Persian Translate; 
+--ch : 
